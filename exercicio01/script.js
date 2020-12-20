3a66d81fe2078ce55ac28639ea637df6a1e8566f
Gustavo Pessoa <gustavo.pessoa@gmail.com>
2020-12-20 17:22:28 +0000

    201220201722

--- exercicio01/script.js
+++ exercicio01/script.js
@@ -1,11 +1,16 @@
 // Escreva uma função que receba dois números e retorne o maior deles
-function maiorNumero() {
+
+function obtemNumeros() {
     let inputN1 = document.getElementById("n1"); 
     let inputN2 = document.getElementById("n2"); 
 
+    maiorNumero(inputN1, inputN2);
+}
+
+function maiorNumero(n1, n2) {
     
-    num1 = inputN1.value;
-    num2 = inputN2.value;
+    num1 = n1.value;
+    num2 = n2.value;
   
     let result = 0;
 
---
+++ exercicio02/script.js
@@ -0,0 +1,22 @@
+// Escreva uma função que receba um Array com 3 números e imprima eles em ordem
+
+function obtemNumeros() {
+    let inputN1 = document.getElementById("n1"); 
+    let inputN2 = document.getElementById("n2");
+    let inputN3 = document.getElementById("n3"); 
+
+    let numeros = [inputN1.value, inputN2.value, inputN3.value];
+    
+    ordenarLista(numeros);
+
+}
+
+function ordenarLista(lista) {
+    
+    var listaOrdenada = lista.sort((a,b)=>a-b)
+    for (let num of listaOrdenada) {
+       console.log(num)
+    }
+   
+}
+    
